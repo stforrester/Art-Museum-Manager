dc7b20a35f5810200c8e26738c0f2cb102231f01
Stuart Forrester <stforrester@gmail.com>
2025-01-03 08:12:50 -0600

    adds initial cli flow diagram and empty cli file

---
+++ cli.py
@@ -0,0 +1,9 @@
+# User stories:
+# As a user, I can:
+# 1. select a menu option to see all the works of art
+# 2. select a menu option to see one work of art's details
+# 3. select a menu option to see all the museums
+# 4. select a menu option to see one museum's details
+# 5. select a menu option to see a museum's art collection
+# 6. select a menu option to add a museum
+# 7. select a menu option to add a work of art
